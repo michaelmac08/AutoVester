d4bfe043bce73d24a7dbc09d1f03950fac2a3f4b
Michael Macer <135725603+michaelmac08@users.noreply.github.com>
2023-06-25 19:53:06 +0800

    Update guardnear.lua

--- guardnear.lua
+++ guardnear.lua
@@ -72,9 +72,9 @@ end
 function cmdAv()
     ActivateAv = not ActivateAv
     if ActivateAv then
-        sampAddChatMessage("{901A00}YO {FFFFFF}| {1E90FF}[Autovest]: {FFFFFF}Auto accept vest has been Enabled")
+        sampAddChatMessage("{1E90FF}[Autovest]: {FFFFFF}Auto accept vest has been Enabled")
     else
-        sampAddChatMessage("{901A00}YO {FFFFFF}| {1E90FF}[Autovest]: {FFFFFF}Auto accept vest has been Disabled")
+        sampAddChatMessage("{1E90FF}[Autovest]: {FFFFFF}Auto accept vest has been Disabled")
     end
     if ActivateAv then
         q.onServerMessage = function(c, s)
